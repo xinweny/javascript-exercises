6a4d12867e08158269b91f159673e5740383bec3
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-17 16:10:29 -0400

    Solve exercise using recursion

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // Recursion method
+    if (num == 0) {
+        return '';
+    } else if (num == 1) {
+        return string;
+    } else {
+        return repeatString(string, num - 1) + string;
+    }
+    
 };
 
 // Do not edit below this line
