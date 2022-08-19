cb149346c349605a64a841a8255472f659dd1bea
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-19 14:50:54 -0400

    Add solution code to sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if (a <= b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
