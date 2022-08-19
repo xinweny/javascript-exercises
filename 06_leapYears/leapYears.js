a2c6b2b2cc98209dd6d2702f2511194bfb915130
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-19 15:22:08 -0400

    Add solution code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!(year % 4)) {
+        return !(year % 100) ? !(year % 400) : true;
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
