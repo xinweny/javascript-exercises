9336bc9b4f324ff68077000bbf684d27b2343611
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-19 15:34:29 -0400

    Add solution code for ctof()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -3,8 +3,9 @@ const ftoc = function(f) {
   return c % 1 === 0 ? c : Number(c.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  const f = (c * 9 / 5) + 32;
+  return f % 1 === 0 ? f : Number(f.toFixed(1));
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.spec.js
+++ 07_tempConversion/tempConversion.spec.js
@@ -13,13 +13,13 @@ describe('ftoc', () => {
 });
 
 describe('ctof', () => {
-  test.skip('works', () => {
+  test('works', () => {
     expect(ctof(0)).toEqual(32);
   });
-  test.skip('rounds to 1 decimal', () => {
+  test('rounds to 1 decimal', () => {
     expect(ctof(73.2)).toEqual(163.8);
   });
-  test.skip('works with negatives', () => {
+  test('works with negatives', () => {
     expect(ctof(-10)).toEqual(14);
   });
 });
