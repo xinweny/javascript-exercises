cef0a967d50b06010489faa3e24df4f0ff7c25d5
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-29 15:53:29 -0400

    Add tested solution code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) {
+    return "OOPS";
+  }
 
+  if (n === 0) {
+    return 0;
+  } else if (n <= 2) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
 
 // Do not edit below this line
