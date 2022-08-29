2f11cd88c473054c3194557260a5e73ea3888b0d
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-29 15:40:03 -0400

    Add tested solution code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const arr = string.toLowerCase() // Convert all letters to lowercase
+              .split('') // Split into array
+              .filter(char => char.match('[a-z]')) // Keep letters only
 
+  const len = arr.length;
+  const midpoint = Math.floor((len / 2) - 1);
+  
+  for (let i = 0; i <= midpoint; i++) {
+    if (arr[i] != arr[len - (i + 1)]) return false;
+  }
+  return true
 };
 
 // Do not edit below this line
