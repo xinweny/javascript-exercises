abcc57027425595cae66a44bb19fa149dec91379
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-29 15:17:51 -0400

    Add tested solution code for factorial()

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,9 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  const nums = [...Array(n + 1).keys()].slice(1);
+	return nums.reduce((f, n) => f *= n, 1);
 };
 
 // Do not edit below this line
