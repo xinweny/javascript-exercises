b09978c8f2a7f7ffd6718f271dbfca15da4552d2
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-29 16:28:55 -0400

    Add tested solution code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = (new Date()).getFullYear();
+  
+  const sortedByAge = people.sort((current, next) => {
+    current.age = (!current.yearOfDeath) ? currentYear - current.yearOfBirth : current.yearOfDeath - current.yearOfBirth;
+    next.age = (!next.yearOfDeath) ? currentYear - next.yearOfBirth : next.yearOfDeath - next.yearOfBirth;
 
+    return next.age - current.age;
+  });
+
+  return sortedByAge[0];
 };
 
 // Do not edit below this line
